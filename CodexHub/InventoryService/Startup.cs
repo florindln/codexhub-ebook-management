ad6ed275711be888269ec3e914fcf44d8d0307cd
florindln <deleanu.f30@gmail.com>
2022-03-27 13:11:41 +0200

    add support for circuitbreaker pattern in case of  synchronous communication

--- CodexHub/InventoryService/Startup.cs
+++ CodexHub/InventoryService/Startup.cs
@@ -6,7 +6,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Timeout;
 using System;
+using System.Net.Http;
 
 namespace InventoryService
 {
@@ -26,7 +30,32 @@ namespace InventoryService
             services.AddHttpClient<BookCatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("http://localhost:5002");
-            });
+            });/*
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                    5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (outcome, timespan, retryAttempt) =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<BookCatalogClient>>()?
+                        .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                    }))
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                    3,
+                    TimeSpan.FromSeconds(15),
+                    onBreak: (outcome, timespan) =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<BookCatalogClient>>()?
+                        .LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds");
+                    },
+                    onReset: () =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<BookCatalogClient>>()?
+                        .LogWarning($"Closing the circuit");
+                    }))
+                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));*/
 
             services.AddControllers().AddNewtonsoftJson().AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null); ;
